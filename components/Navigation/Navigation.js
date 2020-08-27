135e62ed90d57b9ebee67944c3d23c62483000e5
Michael Hale <mail@michaelhale.xyz>
2020-08-26 21:50:49 -0400

    Add 'coming soon' notice

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -2,10 +2,20 @@ import styles from "./Navigation.module.css";
 
 export default function Navigation() {
   return (
-    <header>
-      <h1 className={styles.logo}>
-        Blaseball <span className={styles.hasSmallText}>Reference</span>
-      </h1>
-    </header>
+    <>
+      <header>
+        <h1 className={styles.logo}>
+          Blaseball <span className={styles.hasSmallText}>Reference</span>
+        </h1>
+        <p style={{ textAlign: "center" }}>Coming soon.</p>
+      </header>
+      <p>
+        Visit{" "}
+        <a href="https://dev.blaseball-reference.com" className={styles.link}>
+          dev.blaseball-reference.com
+        </a>{" "}
+        to view the latest updates.
+      </p>
+    </>
   );
 }
