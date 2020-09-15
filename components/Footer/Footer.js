257310a9b3d953a321ebcd4a37b67a43261f49b0
Michael Hale <mail@michaelhale.xyz>
2020-09-15 12:59:11 -0400

    Add Twitter and GitHub links

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,25 +1,60 @@
-import { Box, Container, Link } from "@chakra-ui/core";
+import { Box, Container, Link, List, ListItem, Text } from "@chakra-ui/core";
 
 export default function Footer() {
   return (
     <Container maxWidth="6xl">
       <Box
         as="footer"
-        color="gray.400"
+        color="gray.500"
         fontSize="sm"
         fontStyle="italic"
-        my={{ base: 6, md: 8 }}
+        my={{ base: 8, md: 10 }}
         textAlign="center"
       >
-        Blaseball Reference is neither endorsed by or directly affiliated with{" "}
-        <Link href="https://thegameband.com/" isExternal>
-          The Game Band
-        </Link>{" "}
-        or{" "}
-        <Link href="https://www.sports-reference.com/" isExternal>
-          Sports Reference
-        </Link>
-        .
+        <Box mb={2}>
+          <List>
+            <ListItem display="inline">
+              <Link
+                href="https://twitter.com/blaseball_ref"
+                isExternal
+                textDecoration="underline"
+              >
+                Twitter
+              </Link>
+            </ListItem>
+            <ListItem display="inline" ml={1}>
+              -
+            </ListItem>
+            <ListItem display="inline" ml={1}>
+              <Link
+                href="https://github.com/mjhale/blaseball-reference.com"
+                isExternal
+                textDecoration="underline"
+              >
+                GitHub
+              </Link>
+            </ListItem>
+          </List>
+        </Box>
+        <Box>
+          Blaseball Reference is neither endorsed by or directly affiliated with{" "}
+          <Link
+            href="https://thegameband.com/"
+            isExternal
+            textDecoration="underline"
+          >
+            The Game Band
+          </Link>
+          <Text as="span"> or </Text>
+          <Link
+            href="https://www.sports-reference.com/"
+            isExternal
+            textDecoration="underline"
+          >
+            Sports Reference
+          </Link>
+          .
+        </Box>
       </Box>
     </Container>
   );
