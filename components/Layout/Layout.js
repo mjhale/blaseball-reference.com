69b09c6caa229931f4c9ef1fe28749d9dc5475c8
Michael Hale <mail@michaelhale.xyz>
2020-09-19 21:04:44 -0400

    Add preview build warning and nofollow tag

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,14 @@
-import { Alert, AlertIcon, Container } from "@chakra-ui/core";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  Container,
+  Link,
+  Text,
+} from "@chakra-ui/core";
 import Footer from "components/Footer";
 import Navigation from "components/Navigation";
+import NextLink from "next/link";
 import { SkipNavContent } from "@reach/skip-nav";
 
 export default function Layout({ children }) {
@@ -8,17 +16,50 @@ export default function Layout({ children }) {
     <>
       <Navigation />
       <main>
-        <SkipNavContent />
         <Container maxWidth="6xl">
-          <Alert bgColor="gray.100" mb={4} status="info">
-            <AlertIcon />
-            All stats are currently based on incomplete data.
-          </Alert>
+          <PreviewBuildWarning />
+          <StatDisclaimerNotice />
 
-          {children}
+          <>
+            <SkipNavContent />
+            {children}
+          </>
         </Container>
       </main>
       <Footer />
     </>
+  );
+}
+
+function PreviewBuildWarning() {
+  const DEPLOYMENT_ENV = process.env.NEXT_PUBLIC_DEPLOYMENT_ENV;
+
+  if (DEPLOYMENT_ENV === "production") {
+    return null;
+  }
+
+  return (
+    <Alert mb={4} status="warning">
+      <AlertIcon />
+      <AlertDescription>
+        This is a subdomain intended for previewing unstable features. For the
+        main website, visit{" "}
+        <NextLink href="http://blaseball-reference.com/" passHref>
+          <Link display="inline-block" isExternal textDecoration="underline">
+            www.blaseball-reference.com
+          </Link>
+        </NextLink>
+        .
+      </AlertDescription>
+    </Alert>
+  );
+}
+
+function StatDisclaimerNotice() {
+  return (
+    <Alert bgColor="gray.100" mb={4} status="info">
+      <AlertIcon />
+      All stats are currently based on incomplete data.
+    </Alert>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,14 @@ import { ChakraProvider, CSSReset } from "@chakra-ui/core";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  const DEPLOYMENT_ENV = process.env.NEXT_PUBLIC_DEPLOYMENT_ENV;
+
   return (
     <>
       <Head>
+        {DEPLOYMENT_ENV !== "production" ? (
+          <meta name="robots" content="noindex" />
+        ) : null}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -30,10 +35,6 @@ function MyApp({ Component, pageProps }) {
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          property="og:image"
-          content="https://blaseball-reference.com/default-share-image.jpg"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lora:wght@700&display=swap"
@@ -44,6 +45,11 @@ function MyApp({ Component, pageProps }) {
             __html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
               heap.load(${process.env.NEXT_PUBLIC_HEAP_IO_KEY});`,
           }}
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="https://blaseball-reference.com/default-share-image.jpg"
         />
       </Head>
       <ChakraProvider theme={theme}>
