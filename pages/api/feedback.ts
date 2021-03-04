28d77a23dbc5b80c23a4285ddf6b7c0b98dd20a8
Michael Hale <mail@michaelhale.xyz>
2021-03-04 09:19:02 -0500

    Add errors to feedback responses

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -3,14 +3,21 @@ import faunadb from "faunadb";
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (request: NowRequest, response: NowResponse) => {
+  if (request.method !== "POST") {
+    return response.status(405).json({
+      status: 405,
+      message: `Error: Method ${request.method} not allowed.`,
+    });
+  }
+
   const { emoji, emojiId, page } = request.body;
 
   if (!emoji || !emojiId || !page) {
-    return response
-      .status(400)
-      .send(
-        "Error: Request body missing required `emoji`, `emojiId`, and `page` keys."
-      );
+    return response.status(400).json({
+      status: 400,
+      message:
+        "Error: Request body missing required `emoji`, `emojiId`, and `page` keys.",
+    });
   }
 
   const client = new faunadb.Client({
@@ -24,9 +31,15 @@ export default (request: NowRequest, response: NowResponse) => {
         data: { emoji, emojiId, page },
       })
     )
-    .catch(function (err) {
-      console.log(err);
+    .then(() => {
+      return response
+        .status(200)
+        .json({ status: 200, message: "Feedback submitted." });
+    })
+    .catch(() => {
+      return response.status(500).json({
+        status: 500,
+        message: "Error: Unable to submit feedback due to database response.",
+      });
     });
-
-  response.status(200).send("Feedback submitted.");
 };
