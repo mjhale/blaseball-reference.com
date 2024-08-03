280bafeedc7c691da191544a519a02e8ffe0f19f
Michael Hale <mail@michaelhale.xyz>
2024-08-03 12:59:58 -0400

    Fix build script and remove Heap from README

--- next.config.js
+++ next.config.js
@@ -47,8 +47,8 @@ module.exports = {
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && isServer) {
+  webpack: (config, { dev, isServer, nextRuntime }) => {
+    if (!dev && isServer && nextRuntime === 'nodejs') {
       const originalEntry = config.entry;
 
       config.entry = async () => {
